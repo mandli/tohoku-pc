b40b9206c2c8557d68cb3c72cfbed9ca46b25184
Kyle Mandli <kyle.mandli@gmail.com>
2014-01-02 14:55:21 -0600

    Move over to new batch system

--- run_friction.py
+++ run_friction.py
@@ -5,20 +5,23 @@ import sys
 
 import numpy
 
-import clawpack.clawutil.tests as clawtests
+import batch
 
-
-class FrictionTest(clawtests.Test):
+class FrictionJob(batch.Job):
     r""""""
 
     def __init__(self, index, values, 
-                                contours=(numpy.infty,0.0,-200.0,-numpy.infty)):
+                       source_path='$SRC/tohoku2011-paper1/sources/Ammon.txydz',
+                       contours=(numpy.infty,0.0,-200.0,-numpy.infty)):
         r""""""
 
-        super(FrictionTest, self).__init__()
+        super(FrictionJob, self).__init__()
+
+        self.source_path = os.path.expandvars(source_path)
 
         self.type = "tohoku"
-        self.name = "friction-sensitivity"
+        self.name = "friction-sensitivity-%s" % \
+                        os.path.split(os.path.splitext(self.source_path)[0])[-1]
         self.prefix = "fric_%s" % (str(index))
         self.executable = 'xgeoclaw'
 
@@ -34,11 +37,16 @@ class FrictionTest(clawtests.Test):
                        self.rundata.clawdata.lower, self.rundata.clawdata.upper,
                        contours, values])
 
+        # Set earthquake source model
+        dtopo_data = self.rundata.dtopo_data
+        dtopo_data.dtopofiles.append([1, 4, 4, self.source_path])
+
     def __str__(self):
-        output = super(FrictionTest, self).__str__()
+        output = super(FrictionJob, self).__str__()
+        output += "\n  Source model: %s" % self.source_path
         output += "\n  Friction Regions:"
         for (n,region) in enumerate(self.rundata.friction_data.friction_regions):
-            output += "\n  Region %s: %s" % (n, region)
+            output += "\n    Region %s: %s" % (n, region)
         return output
 
 
@@ -48,13 +56,16 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         path = sys.argv[1]
 
+    source_path = '$SRC/tohoku2011-paper1/sources/Ammon.txydz'
+
     # Read in friction test values
     friction_values = numpy.loadtxt(path)
 
-    # Create all tests
-    tests = []
+    # Create all jobs
+    jobs = []
     for n in xrange(friction_values.shape[0]):
-        tests.append(FrictionTest(n, friction_values[n,:]))
+        jobs.append(FrictionJob(n, friction_values[n,:], source_path))
     
-    controller = clawtests.TestController(tests)
-    controller.run()
+    controller = batch.BatchController(jobs)
+    # controller.run()
+    print controller
